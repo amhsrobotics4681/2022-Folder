a94f640bae806f44072ada1ded10f462f83119a2
Diana <strongcat13@gmail.com>
2022-10-27 17:01:04 -0700

    motor control
    trying to do things with motor controlling and with joystick messing around. also initial commit for my project :]

--- Diana/makeaproject/src/main/java/frc/robot/Robot.java
+++ Diana/makeaproject/src/main/java/frc/robot/Robot.java
@@ -1,14 +1,13 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-cbt is ians favorite topic;
 
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.motorcontrol.Victor;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 import edu.wpi.first.wpilibj.Joystick;
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -20,36 +19,19 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
-  Victor motorL;
-  Victor motorR;
-  Joystick controller;
-PrintCommand "ian was here";
+
+  PWMVictorSPX m_left;
+  PWMVictorSPX m_right;
+  Joystick control;
+  
   @Override
-  public void robotInit() {
-    motorL = new Victor(0);
-    motorR = new Victor(1);
-    controller = new Joystick(0);
-  }
+  public void robotInit() {}
 
   @Override
   public void robotPeriodic() {}
 
   @Override
-  public void autonomousInit() {r motorL;
-    Victor motorR;
-    Joystick controller;
-  
-    @Override
-    public void robotInit() {
-      motorL = new Victor(0);
-      motorR = new Victor(1);
-      controller = new Joystick(0);
-    }
-  
-    @Override
-    public void robotPeriodic() {}
-  
-    @Override}
+  public void autonomousInit() {}
 
   @Override
   public void autonomousPeriodic() {}
@@ -59,8 +41,14 @@ PrintCommand "ian was here";
 
   @Override
   public void teleopPeriodic() {
-    motorL.set(controller.getRawAxis(1));
-    motorR.set(controller.getRawAxis(3));
+
+    if (control.getX() > 0.0) {
+      m_left.set(0.5); // can go from -1 to 1
+    }
+    
+
+
+    
   }
 
   @Override
