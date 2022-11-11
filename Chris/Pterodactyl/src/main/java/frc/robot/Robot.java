634dcb7715b81a61efb318473fc545cd3f3f0d17
Christopher Marley <chrismarley3.14@gmail.com>
2022-11-10 16:27:21 -0800

    CODE

--- Chris/Pterodactyl/src/main/java/frc/robot/Robot.java
+++ Chris/Pterodactyl/src/main/java/frc/robot/Robot.java
@@ -1,34 +1,19 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 import edu.wpi.first.wpilibj.Joystick;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used
-   * for any initialization code.
-   */
-
-  PWMVictorSPX m_left;
-  Joystick c_driver;
-  
+  PWMVictorSPX m_left, m_right;
+  Joystick c_driver, c_shooter;
 
   @Override
   public void robotInit() {
     m_left = new PWMVictorSPX(0);
+    m_right = new PWMVictorSPX(1);
     c_driver = new Joystick(0);
+    c_shooter = new Joystick(1);
   }
 
   @Override
@@ -38,22 +23,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     m_left.set(c_driver.getRawAxis(0));
+    // WRITE CODE SO THAT THE RIGHT MOTOR ACTIVATES IF BUTTON 2 IS HELD DOWN ON THE DRIVER CONTROLLER
+    m_right.set(c_driver.getRawAxis(2));
   }
-
-  @Override
-  public void disabledInit() {
-  }
-
-  @Override
-  public void disabledPeriodic() {
-  }
-
-  @Override
-  public void testInit() {
-  }
-
-  @Override
-  public void testPeriodic() {
-  }
-
 }
