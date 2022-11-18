602023b5337d54f21c1159687d4022d33daf8aa6
amhsrobotics4681 <37252833+amhsrobotics4681@users.noreply.github.com>
2022-11-17 16:46:14 -0800

    wheeeee!!!!!

--- Chris/Pterodactyl/src/main/java/frc/robot/Robot.java
+++ Chris/Pterodactyl/src/main/java/frc/robot/Robot.java
@@ -3,27 +3,19 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.cameraserver.CameraServer;
 
 public class Robot extends TimedRobot {
-  PWMVictorSPX m_left, m_right;
-  Joystick c_driver, c_shooter;
 
   @Override
   public void robotInit() {
-    m_left = new PWMVictorSPX(0);
-    m_right = new PWMVictorSPX(1);
-    c_driver = new Joystick(0);
-    c_shooter = new Joystick(1);
-  }
-
-  @Override
-  public void teleopInit() {
+    
   }
 
   @Override
   public void teleopPeriodic() {
-    m_left.set(c_driver.getRawAxis(0));
-    if (c_driver.getRawButton(2))
-      m_right.set(c_driver.getRawAxis(2));
   }
 }
