83f3540c5608cf0177ed3632889f5380e3266be2
Christopher Marley <chrismarley3.14@gmail.com>
2022-12-01 17:42:04 -0800

    Attempt at arraylist

--- Chris/Pterodactyl/src/main/java/frc/robot/Robot.java
+++ Chris/Pterodactyl/src/main/java/frc/robot/Robot.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 import edu.wpi.first.wpilibj.ADIS16448_IMU;
@@ -12,38 +16,64 @@ import edu.wpi.first.cameraserver.CameraServer;
 
 public class Robot extends TimedRobot {
   PWMVictorSPX m_left, m_right;
+  Joystick m_controller;
   ADIS16470_IMU m_gyro;
   int timer;
 
+  ArrayList<ArrayList<Float>> arr2 = new ArrayList<ArrayList<Float>>();
+  
+
   @Override
   public void robotInit() {
-    m_left = new PWMVictorSPX(0);
-    m_right = new PWMVictorSPX(1);
+    m_right = new PWMVictorSPX(0);
+    m_left = new PWMVictorSPX(1);
+    m_controller = new Joystick(0);
     m_gyro = new ADIS16470_IMU();
+
+    m_right.setInverted(true);
+
+    ArrayList<Float> arr = new ArrayList<>();
+    arr.add((float) 0); arr.add((float) 50.0);arr.add((float) 0.5);arr.add((float) 0);
+    arr2.add(arr);
+    ArrayList<Float> ars = new ArrayList<>();
+    ars.add((float) 50); ars.add((float) 100.0);ars.add((float) 0);ars.add((float) 90);
+    arr2.add(arr);
+    ArrayList<Float> art = new ArrayList<>();
+    art.add((float) 100); art.add((float) 200.0);art.add((float) 0);art.add((float) -180);
+    arr2.add(arr);
+    ArrayList<Float> aru = new ArrayList<>();
+    aru.add((float) 200); aru.add((float) 400.0);aru.add((float) -0.3);aru.add((float) 0);
+    arr2.add(arr);
   }
 
   @Override
   public void autonomousInit() {
+    m_gyro.reset();
     timer = 0;
   }
 
   @Override
   public void autonomousPeriodic() {
-    if (timer < 50) {
-      m_left.set(1);
-      timer++;
-    } else if (timer < 100) {
-      m_left.set(0);
-      m_right.set(1);
-      timer++;
-    } else {
-      m_left.set(0);
-      m_right.set(0);
+    timer++;
+    for (ArrayList<Float> arrf : arr2) {
+      if (timer < arrf.get(1) && timer > arrf.get(0)) {
+        if (arrf.get(2) != 0) {
+          m_left.set(arrf.get(2));
+          m_right.set(arrf.get(2));
+        } else {
+          if (m_gyro.getAngle() < arrf.get(3)) {
+            m_left.set(1);
+          }
+        }
+      }
+      System.out.print(arrf);
     }
   }
 
   @Override
   public void teleopPeriodic() {
+    m_right.set(-m_controller.getRawAxis(3));
+    m_left.set(-m_controller.getRawAxis(1));
     System.out.println(m_gyro.getAngle());
   }
 }
