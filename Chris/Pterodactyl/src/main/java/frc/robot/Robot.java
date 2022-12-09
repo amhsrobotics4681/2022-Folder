92dbf45edf7c1d25ca6566bf7de4bbb937391e9d
Christopher Marley <chrismarley3.14@gmail.com>
2022-12-08 16:32:17 -0800

    sendablechooser

--- Chris/Pterodactyl/src/main/java/frc/robot/Robot.java
+++ Chris/Pterodactyl/src/main/java/frc/robot/Robot.java
@@ -12,6 +12,8 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser; // <--------
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.cameraserver.CameraServer;
 
 public class Robot extends TimedRobot {
@@ -19,9 +21,7 @@ public class Robot extends TimedRobot {
   Joystick m_controller;
   ADIS16470_IMU m_gyro;
   int timer;
-
-  ArrayList<ArrayList<Float>> arr2 = new ArrayList<ArrayList<Float>>();
-  
+  SendableChooser<Double> m_chooser;
 
   @Override
   public void robotInit() {
@@ -29,45 +29,32 @@ public class Robot extends TimedRobot {
     m_left = new PWMVictorSPX(1);
     m_controller = new Joystick(0);
     m_gyro = new ADIS16470_IMU();
+    m_chooser = new SendableChooser<>();
+
+    m_chooser.setDefaultOption("default", 0.2);
+    m_chooser.addOption("option 1", 0.3);
+    m_chooser.addOption("option 2", 0.4);
+    m_chooser.addOption("option 3", -0.3);
+    m_chooser.addOption("option 4", -0.2);
 
     m_right.setInverted(true);
-
-    ArrayList<Float> arr = new ArrayList<>();
-    arr.add((float) 0); arr.add((float) 50.0);arr.add((float) 0.5);arr.add((float) 0);
-    arr2.add(arr);
-    ArrayList<Float> ars = new ArrayList<>();
-    ars.add((float) 50); ars.add((float) 100.0);ars.add((float) 0);ars.add((float) 90);
-    arr2.add(arr);
-    ArrayList<Float> art = new ArrayList<>();
-    art.add((float) 100); art.add((float) 200.0);art.add((float) 0);art.add((float) -180);
-    arr2.add(arr);
-    ArrayList<Float> aru = new ArrayList<>();
-    aru.add((float) 200); aru.add((float) 400.0);aru.add((float) -0.3);aru.add((float) 0);
-    arr2.add(arr);
+    SmartDashboard.putData(m_chooser);
   }
 
   @Override
   public void autonomousInit() {
     m_gyro.reset();
     timer = 0;
+
   }
 
   @Override
   public void autonomousPeriodic() {
     timer++;
-    for (ArrayList<Float> arrf : arr2) {
-      if (timer < arrf.get(1) && timer > arrf.get(0)) {
-        if (arrf.get(2) != 0) {
-          m_left.set(arrf.get(2));
-          m_right.set(arrf.get(2));
-        } else {
-          if (m_gyro.getAngle() < arrf.get(3)) {
-            m_left.set(1);
-          }
-        }
-      }
-      System.out.print(arrf);
-    }
+    double data = m_chooser.getSelected();
+    System.out.println(data);
+    m_left.set(data);
+    m_right.set(data);
   }
 
   @Override
